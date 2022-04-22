ab661dcbb06981851f7e0afe5654a2b74582b27e
Oguz <oguz2863@gmail.com>
2022-04-22 10:57:52 +0200

    Initial project init

---
+++ ST4-ImplementationExamples/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ST4_ImplementationExamples
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //REST
+            REST rest = new REST();
+            _ = rest.RunExample();
+
+            //MQTT
+            MQTT mqtt = new MQTT();
+            _ = mqtt.RunExample();
+
+            //SOAP
+            SOAP soap = new SOAP();
+            _ = soap.RunExample();
+
+            Console.ReadKey();
+        }
+    }
+}
