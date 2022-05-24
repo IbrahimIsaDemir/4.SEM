cc30edc02b1de77b09d081f6bed54499b362e260
Oguz <oguz2863@gmail.com>
2022-05-24 12:32:37 +0200

    Removed commented code + added warehouse to program

--- ST4-ImplementationExamples/Program.cs
+++ ST4-ImplementationExamples/Program.cs
@@ -12,77 +12,11 @@ namespace ST4_ImplementationExamples
             MQTT mqtt = new MQTT();
             SOAP soap = new SOAP();
             //_ = rest.RunExample();
-            /*
+
             
-            //MQTT
-            _ = mqtt.RunExample();
-            Console.WriteLine("Select an option.");
-            Console.WriteLine("1> Idle");
-            Console.WriteLine("2> Executing");
-            Console.WriteLine("3> Error");
-            Console.WriteLine("4> gogo");
-            Console.WriteLine("5> Exit");
-            while (true)
-            {
-                string input = Console.ReadLine();
-                int number;
-                Int32.TryParse(input, out number);
-                
-                switch (number)
-                {
-                    case 1:
-                    {
-                      mqtt.Idle();
-                        
-                        
-                    }
-                        break;
-                    case 2:
-                    {  // stand in execution state
-                        mqtt.Execution();
-                        break;
+            //Warehouse
+            soap.PickAndInsertItem();
 
-                    }
-                    case 3:
-                    {
-                        mqtt.Error();
-                        break;
-                    }
-                    case 4:
-                    {
-                        mqtt.UnsubscribeAsync("emulator/status");
-                        Console.WriteLine("go");
-                        break;
-                    }
-                    case 5:
-                    {
-                        Environment.Exit(0);
-                        break;
-                    }
-                  
-                    default:
-                    {
-                        mqtt.UnsubscribeAsync("emulator/status");
-                        Console.WriteLine("Invalid Input!");
-                        break;
-                    }
-                }
-            }
-
-            //SOAP
-            _ = soap.RunExample();
-            */
-            /*
-            Console.WriteLine("Welcome to the AGV");
-            Console.WriteLine("Please pick a program");
-            Console.WriteLine("1: move to warehouse");
-            Console.WriteLine("2: Pick items from warehouse");
-            Console.WriteLine("3: put items into warehouse");
-            Console.WriteLine("4: move to assembly station");
-            Console.WriteLine("5: put items into assemblystation");
-            Console.WriteLine("6: pick items from assembly station");
-            Console.WriteLine("7: Move to charger");
-            */
             rest.ChooseOperation(1);
             rest.GetStatus();
             rest.execute();
@@ -106,9 +40,10 @@ namespace ST4_ImplementationExamples
             rest.GetStatus();
             Thread.Sleep(8000);
             rest.GetStatus();
-            
+
             //Her skal koden for assemblyStation sættes ind
             
+
             rest.ChooseOperation(6);
             rest.GetStatus();
             rest.execute();
@@ -125,28 +60,6 @@ namespace ST4_ImplementationExamples
             rest.execute();
             Thread.Sleep(8000);
             rest.GetStatus();
-            
-            //Her skal warehouse koden sættes ind
-
-            /*
-            while (true)
-            {
-                int i = int.Parse(Console.ReadLine());
-                int maximumOptions = 7;
-                
-                if (i>maximumOptions)
-                {
-                    Console.WriteLine($"Please pick a value between 1 and {maximumOptions}");
-                }
-                
-                if (i<=maximumOptions)
-                {
-                    rest.ChooseOperation(i);
-                    //rest.GetStatus();
-                }
-                Console.ReadKey();
-            }
-            */
         }
     }
 }
